10fcab0a0e8b86d6bc3d379fe36b9c8cb0944435
Lucchetto <lucchetto.tie@live.com>
2018-03-03 13:41:39 +0100

    Wrong libs version
    Change-Id: Icd1594c9f9870142224a81d181940655402bde4c

--- cc/ndk_prebuilt.go
+++ cc/ndk_prebuilt.go
@@ -161,7 +161,7 @@ func ndkPrebuiltStaticStlFactory() android.Module {
 }
 
 func getNdkStlLibDir(ctx android.ModuleContext, toolchain config.Toolchain, stl string) android.SourcePath {
-	gccVersion := "7.x"
+	gccVersion := "4.9"
 	var libDir string
 	switch stl {
 	case "libstlport":
